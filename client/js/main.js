3afd719b962c7b9048745181e902c43472b20d5f
Jonathan Arnett <jonarnett90@gmail.com>
2015-03-04 16:46:18 -0500

    All links work

--- client/js/main.js
+++ client/js/main.js
@@ -5,12 +5,16 @@ $(document).ready(function() {
         section: ".section"
     });
 
+    $(".brand_image").click(function(){
+        $.scrollify("move", 0);
+    })
+
     $("#about-link").click(function() {
         $.scrollify("move", 1)
     });
 
     $("#events-link").click(function() {
-        $.scrollify("move", 2)
+        $.scrollify("move", 3)
     });
 
     $("#news-link").click(function() {
