2eb80b70cfd81aec2b044866d98fc4f54b9b01ce
afuhrtrumpet@gmail.com <afuhrtrumpet@gmail.com>
2015-03-31 20:17:27 -0700

    Added scrollsnap, not really working

--- client/js/main.js
+++ client/js/main.js
@@ -1,7 +1,9 @@
 Template.main.rendered = function() {
-    $(".section").height($(window).height());
-
-    $.scrollify({
-        section: ".section"
-    });
-};
+	$(".section").height($(window).height());
+	$('#content').scrollsnap({
+		snaps: '.section',
+		proximity: 400,
+		easing: 'easeOutBack'
+	});
+	console.log("Scrollsnap activated");
+}
