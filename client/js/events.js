9a5edcca5d20399c8b5f6ea60e53a82d782545cf
tsujibiha <arrowshaft@gmail.com>
2015-04-01 17:56:58 -0400

    Undeprecated the code

--- client/js/events.js
+++ client/js/events.js
@@ -1,5 +1,7 @@
 Meteor.subscribe("events");
 
-Template.club_events.recentEvents = function() {
+Template.club_events.helpers({
+	recentEvents: function() {
 	return Events.find({}, {limit: 2}, {sort: {date: -1}});
-};
+}
+});
