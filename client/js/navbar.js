094ba324e152e2deb5b66452409e7864bbe52953
afuhrtrumpet@gmail.com <afuhrtrumpet@gmail.com>
2015-03-31 18:39:06 -0700

    Added full sidebar transition

--- client/js/navbar.js
+++ client/js/navbar.js
@@ -21,3 +21,10 @@ Template.navbar.helpers({
 		return Meteor.user() && Meteor.user().roles && Meteor.user().roles.indexOf('admin') >= 0;
 	}
 });
+
+Template.titlepage.rendered = function() {
+	$('.fa-bars').click(function() {
+		console.log("Bars clicked");
+		$('body').toggleClass('menu-active');
+	});
+};
