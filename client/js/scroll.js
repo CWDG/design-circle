a79cdbe92ea2c49abea7d1cb30cfb5c65f38de07
Jonathan Arnett <jonarnett90@gmail.com>
2015-04-14 12:16:50 -0400

    Pages are full-sized, titles are not centered

--- client/js/scroll.js
+++ client/js/scroll.js
@@ -1,12 +1,30 @@
 Template.main.rendered = function() {
     $('a[href^=#]').on("click", function() {
         var selector = $(this).attr('href');
-        var tOffset = $(selector).offset().top;
-        console.log(tOffset);
+
+        // Debugging
+        console.log("Selector: ", selector);
+
+        // TODO:
+        // NO LONGER WORKS
+        // The location of each selector is relative to the window.
+
+        // Get location of selector
+        var tOffset = $(selector).position();
+        var sectLeft = tOffset.left;
+        var sectTop = tOffset.top;
+
+        // Debugging
+        console.log("Left: ", sectLeft, " Top: ", sectTop);
+
+        // Navigate
         $('html,body').animate({
-            scrollTop: tOffset
+            scrollLeft: sectLeft,
+            scrollTop: sectTop
         },
         'slow');
+
+        // Do not do default action
         return false;
     });
 }
