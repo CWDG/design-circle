77071bf657b7e64366b8bfc595f8fcaa731a032b
Jonathan Arnett <jonarnett90@gmail.com>
2015-04-14 14:35:08 -0400

    Removed debugging

--- client/js/scroll.js
+++ client/js/scroll.js
@@ -2,22 +2,12 @@ Template.main.rendered = function() {
     $('a[href^=#]').on("click", function() {
         var selector = $(this).attr('href');
 
-        // Debugging
-        console.log("Selector: ", selector);
-
-        // TODO:
-        // NO LONGER WORKS
-        // The location of each selector is relative to the window.
-
         // Get location of selector
         var tOffset = $(selector).position();
         var base = $("#penultimate-container").position().left;
 
         var sectLeft = (-base) + tOffset.left;
         var sectTop = tOffset.top;
-
-        // Debugging
-        console.log("Left: ", sectLeft, " Top: ", sectTop);
 
         // Navigate
         $('html,body').animate({
