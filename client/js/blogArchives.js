8c788ebe880cc647c98a02e84a54df06671e0654
Jonathan Arnett <jonarnett90@gmail.com>
2015-04-14 14:36:09 -0400

    More blog fixes

--- client/js/blogArchives.js
+++ client/js/blogArchives.js
@@ -1,17 +1,11 @@
 Meteor.subscribe("blogposts");
 Meteor.subscribe("images");
 
-Template.blogLanding.helpers({
+Template.blog.helpers({
     recentBlogPosts: function() {
-        return BlogPosts.find({}, {limit: 2, sort: {date: -1}});
+        return BlogPosts.find({}, {sort: {date: -1}});
     },
     getBlogPostImage: function(imageId) {
         return Images.findOne(imageId).url();
-    }
-});
-
-Template.blogArchive.helpers({
-    blogPosts: function() {
-        return BlogPosts.find({}, {limit: 7}, {sort: {date: -1}});
     }
 });
