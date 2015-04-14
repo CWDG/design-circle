d0a9e5edc6e430795f6cc373c6deede0ee60a974
afuhrtrumpet@gmail.com <afuhrtrumpet@gmail.com>
2015-04-14 06:32:35 -0700

    Added API key and stuff, nothing happening so far

--- client/js/events.js
+++ client/js/events.js
@@ -2,6 +2,18 @@ Meteor.subscribe("events");
 
 Template.club_events.helpers({
 	recentEvents: function() {
-	return Events.find({}, {limit: 2, sort: {date: -1}});
-}
+		return Events.find({}, {limit: 2, sort: {date: -1}});
+	}
+});
+
+Template.calendar.helpers({
+	calendarOptions: function() {
+		return {
+			googleCalendarApiKey: "AIzaSyDb0rYc_N4TzrfE-i19ftuBnt8DVyz0J30",
+			events: {
+				googleCalendarId: "ohiostatedesigncircle@gmail.com",
+				className: 'gcal-event'
+			}
+		}
+	}
 });
